13329959def5b4f29fcc67bfd9bb96cbb957c046
Jean Luc Ndashimye <ndas0001@algonquinlive.com>
2024-01-22 23:54:58 -0600

    Demo #1

---
+++ Week2/Week2.cpp
@@ -0,0 +1,33 @@
+﻿// Week2.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include "Week2.h"
+
+using namespace std;
+
+namespace CST8219 {
+
+	Vehicle::Vehicle(int w, int d) : numWheels(w), numDoors(d) {
+		cout << "In constructor with 2 parameters" << endl;
+	}
+
+	Vehicle::Vehicle(int w) : Vehicle(w, 4) {
+		cout << "In constructor with 1 parameter, wheels = " << w << endl;
+	}
+
+	Vehicle::Vehicle() : Vehicle(4) {
+		cout << "In construction with 0 parameters" << endl;
+	}
+
+	Vehicle::~Vehicle() {
+		cout << "In destructor" << endl;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	CST8219::Vehicle myVehicle(4);
+	cout << "I made a Vehicle" << endl;
+	return 0;
+}
---
+++ Week2/Week2.h
@@ -0,0 +1,22 @@
+﻿// Week2.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+namespace CST8219 {
+
+	class Vehicle {
+	private:
+		int numWheels;
+		int numDoors;
+
+	public:
+		Vehicle(int w, int d);
+		Vehicle(int w);
+		Vehicle();
+		~Vehicle();
+	};
+}
