e800116baec4bee9a55a40fded58ec545053e724
Jean Luc Ndashimye <ndas0001@algonquinlive.com>
2024-02-05 01:08:16 -0600

    Finished Week4

--- Week2/Week2.cpp
+++ Week2/Week2.cpp
@@ -1,100 +1,32 @@
-﻿#include<iostream>
-using namespace std;
+﻿// Week2.cpp : Include file for standard system include files,
+// or project specific include files.
 
+#include "Vehicle.h"
 
-class Vehicle {
+// Function declarations
+void CreateVehicle(Vehicle& v, int w = 4, int d = 2);
 
-    private:
-        int numWheels;
-        int numDoors;
-    public:
-        Vehicle(int w, int d) {
-            numWheels = w;
-            numDoors = d;
-            cout << "In constructor with 2 parameters" << endl;
-        };
-        Vehicle(int w) : Vehicle(w, 4) {
-            cout << "In constructor with 1 parameters, wheels=" << w << endl;
-        };
-        Vehicle() : Vehicle(4) {
-            cout << "In Constructor with 0 parameters " << endl;
-        };
-        ~Vehicle() {
-            cout << "In destructor" << endl;
-        };
-    };
-int main(int argc, char** argv) {
-    // Create a new variable
-    Vehicle veh1; // This calls constructor Vehicle()
-    Vehicle veh2(4); //This calls constructor Vehicle(int);
-    Vehicle veh3(4, 2); //This calls constructor Vehicle( int, int);
-    cout << "Vehicle takes " << sizeof(veh1) << endl;
-    cout << "Vehicle takes " << sizeof(veh2) << endl;
-    cout << "Vehicle takes " << sizeof(veh2) << endl;
-    Vehicle* pVehicle = nullptr;
+int main() {
+    Vehicle original;                // empty constructor, no ( )
+    Vehicle copy(original);          // copy constructor by reference
+    Vehicle secondCopy(&original);   // copy constructor by pointer
 
-    char choice;
-    do {
-        // Ask the user if they want to create a new vehicle or quit
-        cout << "Do you want to create a new vehicle? (y for yes, q for quit): ";
-        cin >> choice;
+    copy.printVehicle();
 
-        if (choice == 'q') {
-            break;
-        }
-        else if (choice == 'y') {
-            // Delete the memory allocated for the previous object (if any)
-            if (pVehicle != nullptr) {
-                delete pVehicle;
-            }
+    CreateVehicle(copy, 2);          // wheels is 2, everything else is default value
+    copy.printVehicle();
 
-            // Ask the user to input the doors and wheels to create a new Vehicle
-            int d, w;
-            do {
-                cout << "Enter number of doors: ";
-                cin >> d;
+    CreateVehicle(copy, 2, 3);       // wheels is 2, doors is 3
+    copy.printVehicle();
 
-                // Check for invalid input (non-integer)
-                if (cin.fail()) {
-                    cin.clear(); // Clear the error flag
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
-                    cout << "Invalid input. Please enter a valid integer." << endl;
-                    continue;
-                }
+    copy = secondCopy;
+    copy.printVehicle();              // copy is the same as the second copy
 
-                cout << "Enter number of wheels: ";
-                cin >> w;
-
-                // Check for invalid input (non-integer)
-                if (cin.fail()) {
-                    cin.clear(); // Clear the error flag
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
-                    cout << "Invalid input. Please enter a valid integer." << endl;
-                    continue;
-                }
-
-                // Validate the input
-                if (d >= 0 && w > 0) {
-                    break;
-                }
-                else {
-                    cout << "Invalid input. Please enter valid numbers." << endl;
-                }
-            } while (true);
-
-            // Create the object using the new keyword
-            pVehicle = new Vehicle(w, d);
-        }
-        else {
-            cout << "Invalid choice. Please enter 'y' to create a new vehicle or 'q' to quit." << endl;
-        }
-    } while (true);
-
-    // Delete the memory allocated for the last object
-    if (pVehicle != nullptr) {
-        delete pVehicle;
-    }
-
-    // Objects veh1, veh2, and veh3 get deleted after the return 0 line
     return 0;
+}
+
+// Function definitions
+
+void CreateVehicle(Vehicle& v, int w, int d) {
+    v = Vehicle(w, d);
 }
--- Week2/Week2.h
+++
@@ -1,7 +0,0 @@
-﻿// Week2.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
