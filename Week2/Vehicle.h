47985c998e977d6c856fe27f2b60a2f2c739a8e7
Jean Luc Ndashimye <ndas0001@algonquinlive.com>
2024-02-05 01:16:35 -0600

    Finished Week4

---
+++ Week2/Vehicle.h
@@ -0,0 +1,25 @@
+﻿// Vehicle.h : Include file for standard system include files,
+// or project specific include files.
+#pragma once
+
+#include <iostream>
+
+class Vehicle {
+private:
+    int wheels;
+    int doors;
+
+public:
+    // Constructors
+    Vehicle() : wheels(0), doors(0) {}
+    Vehicle(int wheels, int doors) : wheels(wheels), doors(doors) {}
+
+    // Copy Constructors
+    Vehicle(const Vehicle& copy) : Vehicle(copy.wheels, copy.doors) {}
+    Vehicle(const Vehicle* copy) : Vehicle(*copy) {}
+
+    // Print function
+    void printVehicle() const {
+        std::cout << "Wheels: " << wheels << ", Doors: " << doors << std::endl;
+    }
+};
